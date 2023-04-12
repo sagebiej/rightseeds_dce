d4c6ec3ec68523add99bfb8583722f6af25382b6
sagebiej <sagebiel@tu-berlin.de>
2023-04-12 19:50:54 +0200

    more lc automatisation

--- scripts/lcmodels/2lc_nocov.R
+++ scripts/lcmodels/2lc_nocov.R
@@ -3,14 +3,22 @@
 rm(list = ls(, pattern = "^apollo_"))
 
 
+
+
+
 database <- database2
+
+
+
+fixednames  <- c("asc"  , "cost" , "EUBIO" , "BIOVER"  ,"REG" ,  "DE",    "ESP",   "EIGENT" ,    "EIGENTxOC","delta" )
+n_classes=2
 
 apollo_initialise()
 
 ### Set core controls
 apollo_control = list(
-  modelName  ="lc2cl",
-  modelDescr ="2 Class LCL model for Tomato DCE",
+  modelName  =paste0("lc_",n_classes,"class"),
+  modelDescr =paste0(n_classes," Class LCL model for Tomato DCE"),
   indivID    ="RID",
   nCores     = 1, 
     outputDirectory = "modeloutput/lclogit/"
@@ -19,91 +27,41 @@ apollo_control = list(
 # ################################################################# #
 #### DEFINE MODEL PARAMETERS                                     ####
 # ################################################################# #
-
 ### Vector of parameters, including any that are kept fixed in estimation
 
-lcnames <- function(apollobeta, classnames) {
-  
-  x<-NULL
-  
-  for (i in classnames) {
-    g <- gsub("$", paste0("_",i),names(apollo_beta))
-    x<-c(x,g)
-  }
-  
-  apollo_beta <- rep(apollo_beta, length(classnames))
-  
-  names(apollo_beta) <- x 
-  
-  return(apollo_beta)
-  
-}
-
-
-randnames  <- c()  
-fixednames  <- c("asc"  , "cost" , "EUBIO" , "BIOVER"  ,"REG" ,  "DE",    "ESP",   "EIGENT" ,    "EIGENTxOC","delta" )  
-
-beta <- vector()
-
-for (n in randnames) {
-  beta[paste0("mean_",n)]=0
-  beta[paste0("sd_",n)]=0
-  
-}
-
-for (n in fixednames) {
-
-  beta[n]= 0     
-
-}
-
-apollo_beta<-beta
-
-apollo_beta<-lcnames(apollo_beta, c("a","b"))
+beta <-as.vector(t(outer(fixednames, letters[seq_along(1:n_classes)], paste, sep="_"))) 
+apollo_beta= rep(0,length(beta))
+names(apollo_beta) <-beta
 
 ### Vector with names (in quotes) of parameters to be kept fixed at their starting value in apollo_beta, use apollo_beta_fixed = c() if none
 apollo_fixed = c("delta_a")
 
 # ################################################################# #
 #### DEFINE LATENT CLASS COMPONENTS                              ####
-# ################################################################# #
+# ####################################################  
+ 
 apollo_lcPars=function(apollo_beta, apollo_inputs){
+
+
+ attrib  <- fixednames[-grep("delta",fixednames)]
+  lcpars <- list()
   
-  lcpars = list()
-
- n_classes <- 2
-  fixednames  <- c("asc"  , "cost" , "EUBIO" , "BIOVER"  ,"REG" ,  "DE",    "ESP",   "EIGENT" ,    "EIGENTxOC")  
-
-  
-
   for (f in fixednames) {
     lcpars[[f]] <- map(seq_along(1:n_classes), ~ get(paste0(f, "_", letters[.])))
-
-}
-
-  
+    
+  }
 
   
   
-
-  
-  
-
-  # lcpars[["asc"]] = list(asc_a, asc_b)
-  #  lcpars[["cost"]] = list(cost_a, cost_b)
-  # lcpars[["EUBIO"]] = list(EUBIO_a, EUBIO_b)
-  # lcpars[["BIOVER"]] = list(BIOVER_a, BIOVER_b)
-  # lcpars[["REG"]]    = list(REG_a, REG_b)
-  # lcpars[["DE"]]    = list(DE_a, DE_b)
-  # lcpars[["ESP"]] = list(ESP_a, ESP_b)
-  # lcpars[["EIGENT"]] = list(EIGENT_a, EIGENT_b)
-  # lcpars[["EIGENTxOC"]] = list(EIGENTxOC_a, EIGENTxOC_b)
-
-
   
   V=list()
-  V[["class_a"]] = delta_a
-  V[["class_b"]] = delta_b
+  
+  for (c in letters[seq_along(1:n_classes)]) {
+    V[paste0("class_",c)] = get(paste0("delta_",c))
+    
+  }
+  # V[["class_a"]] = delta_a
+  # V[["class_b"]] = delta_b
 
   
   mnl_settings = list(
@@ -160,15 +118,26 @@ apollo_probabilities=function(apollo_beta, apollo_inputs, functionality="estimat
     ### Compute class-specific utilities
      V=list()
 
-    
+
     V[['alt1']] = -cost[[s]]*(-a1_x1 +
-                           asc[[s]] + 
+                           asc[[s]] +
                            EUBIO[[s]]*a1_EUBIO + BIOVER[[s]]*a1_BIOVER + REG[[s]]*a1_REG + DE[[s]]*a1_DE + ESP[[s]]*a1_ESP  + EIGENT[[s]]*a1_x4 +EIGENTxOC[[s]]*a1_x4*oc)
-      
+
         V[['alt2']] = -cost[[s]]*(-a2_x1 +
-                              asc[[s]] + 
-                              EUBIO[[s]]*a2_EUBIO + BIOVER[[s]]*a2_BIOVER + REG[[s]]*a2_REG + DE[[s]]*a2_DE + ESP[[s]]*a2_ESP  + EIGENT[[s]]*a2_x4 +EIGENTxOC[[s]]*a2_x4*oc) 
-    
+                              asc[[s]] +
+                              EUBIO[[s]]*a2_EUBIO + BIOVER[[s]]*a2_BIOVER + REG[[s]]*a2_REG + DE[[s]]*a2_DE + ESP[[s]]*a2_ESP  + EIGENT[[s]]*a2_x4 +EIGENTxOC[[s]]*a2_x4*oc)
+
+     
+     
+     # V[['alt1']] = cost[[s]]*a1_x1 +
+     #                             asc[[s]] + 
+     #                             EUBIO[[s]]*a1_EUBIO + BIOVER[[s]]*a1_BIOVER + REG[[s]]*a1_REG + DE[[s]]*a1_DE + ESP[[s]]*a1_ESP  + EIGENT[[s]]*a1_x4 +EIGENTxOC[[s]]*a1_x4*oc
+     # 
+     # V[['alt2']] = cost[[s]]*a2_x1 +
+     #                             asc[[s]] + 
+     #                             EUBIO[[s]]*a2_EUBIO + BIOVER[[s]]*a2_BIOVER + REG[[s]]*a2_REG + DE[[s]]*a2_DE + ESP[[s]]*a2_ESP  + EIGENT[[s]]*a2_x4 +EIGENTxOC[[s]]*a2_x4*oc 
+     # 
+     # 
     V[['alt3']] = 0 
     
     
